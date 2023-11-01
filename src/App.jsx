3c720bf2d7cd19d68db74db60ceee3209367d096
Juan Alcoba <juanchoalcoba@gmail.com>
2023-11-01 19:08:27 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 
-import './App.css'
 
 function App() {
-
   return (
     <>
-      <h1>Esta es mi primer aplicacion en la web con dominio</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius ratione nam nulla minus exercitationem? Explicabo, neque delectus alias recusandae inventore quis voluptas architecto? Nihil aspernatur iusto magnam veniam ipsam dolore, hic beatae quis sed adipisci, at rem exercitationem? Natus ipsum eius vitae totam perferendis aperiam cupiditate nam rerum quia? Mollitia voluptates soluta consequuntur vel sequi nulla. Nostrum molestiae nam, iusto corrupti deserunt nulla distinctio architecto? Nesciunt dolor nobis facilis dignissimos, maxime quia ratione corporis deleniti sunt reiciendis ut possimus amet numquam voluptatem officiis cum quidem corrupti sapiente laudantium officia porro! Quia laudantium beatae quas cupiditate suscipit cumque culpa, reiciendis saepe?</p>
+      <div className="flex flex-col w-[60rem] m-auto text-white justify-center items-center">
+        <h1 className='text-white text-5xl'>UruguayTec</h1>
         
+      </div>
     </>
   )
 }
---
+++ src/components/ImagenAnimada.jsx
@@ -0,0 +1,18 @@
+
+const ImagenAnimada = () => {
+    return (
+        <div className="flex items-center justify-around mt-24">
+            <div className="text-white">
+                <h1 className="text-8xl">URUGUAY <br/> <span>TECNOLOGIA</span></h1>
+                <p className="w-[500px] mt-8 text-xl">Bievenidos al primer sitio uruguayo donde la tecnología esta en un mismo lugar!</p>
+            </div>
+
+
+            <div>
+                <img className="w-[400px] rounded-xl border border-white" src="/1.jpg" alt="" />
+            </div>
+        </div>
+    )
+}
+
+export default ImagenAnimada
