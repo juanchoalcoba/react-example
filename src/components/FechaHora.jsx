4e60bae595dd00b73683ef33f47ab3688adf5c98
Juan Alcoba <juanchoalcoba@gmail.com>
2023-11-05 17:32:32 -0300

    update

---
+++ src/components/FechaHora.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+
+const FechaHoraActual = () => {
+  const [fechaHora, setFechaHora] = useState(new Date());
+  const [hovered, setHovered] = useState(false);
+
+  useEffect(() => {
+    let frameId;
+
+    const updateFechaHora = () => {
+      setFechaHora(new Date());
+      frameId = requestAnimationFrame(updateFechaHora);
+    };
+
+    frameId = requestAnimationFrame(updateFechaHora);
+
+    return () => cancelAnimationFrame(frameId);
+  }, []);
+
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' };
+  const fechaHoraFormateada = fechaHora.toLocaleDateString('es-ES', options).toUpperCase();
+  const fechaHoraFormateadaEn = fechaHora.toLocaleDateString('en-US', options).toUpperCase();
+
+  return (
+    <div
+      className="border border-white flex flex-col items-center w-[500px] justify-center ml-36 bg-gray-200 bg-opacity-10 text-white px-8 rounded-xl"
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+    >
+      <p className="text-xl text-blue-200">
+        {hovered ? fechaHoraFormateadaEn : fechaHoraFormateada}
+      </p>
+    </div>
+  );
+}
+
+export default FechaHoraActual;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,19 @@
+import FechaHoraActual from "./FechaHora"
 
 const Navbar = () => {
   return (
     <div className="flex flex-row justify-around">
         <div className="flex flex-row">
             <img src="/12.png" className='w-[120px] rounded-xl opacity-3' alt="" />
-            <img src="/gif.gif" className='w-[70px] opacity-7 ml-12' alt="" />
         </div>
        
         <div className="text-white">
-            <ul className="flex cursor-pointer flex-row justify-center font-light text-[1.5rem] items-center">
+            <ul className="flex cursor-pointer flex-row justify-center font-semibold text-[1.5rem] items-center">
                 <li className="mr-8">CONTACTO</li>
                 <li>SOBRE MI</li>
             </ul>
         </div>
+                <FechaHoraActual />
       
     </div>
   )
