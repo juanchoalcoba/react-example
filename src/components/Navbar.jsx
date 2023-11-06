14850352ff1d0aee4732fa06488881b8a42238bf
Juan Alcoba <juanchoalcoba@gmail.com>
2023-11-06 11:36:47 -0300

    udate

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,11 +4,11 @@ const Navbar = () => {
   return (
     <div className="flex flex-row justify-around">
         <div className="flex flex-row">
-            <img src="/12.png" className='w-[80px] rounded-xl opacity-3' alt="" />
+            <img src="/12.png" className='w-[60px] rounded-xl opacity-3' alt="" />
         </div>
        
         <div className="text-white">
-            <ul className="flex cursor-pointer flex-row justify-center font-semibold text-[1.5rem] items-center">
+            <ul className="flex cursor-pointer flex-row justify-center font-light underline hover:text-[#77b7db] text-[1.5rem] items-center">
                 <li className="mr-8">CONTACTO</li>
                 <li>SOBRE MI</li>
             </ul>
